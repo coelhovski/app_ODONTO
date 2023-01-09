612fa5d933e3bb0395b3a422a4795fbb7f3f73ee
coelhovski <101751528+coelhovski@users.noreply.github.com>
2023-01-09 11:03:29 -0300

    tela_inicial_prototipo
    Criação em HTML da tela inicial

--- index.php/index.php
+++ index.php/index.php
@@ -3,5 +3,27 @@
     <title>Página Inicial</title>
 </head>
 <body>
-    <h1>TESTE</h1>
+    <header>
+    </header>
+    <body>
+        <article class="article">
+            <h4>LOGIN - PROFESSORES</h4>
+            <form action="#" method="POST" class="form_login">
+                <table>
+                    <tr>
+                        <td>icone</td>
+                        <td><input type="text" id="login" name="login" placeholder="login"></td>
+                    </tr>
+                    <tr>
+                        <td>icone</td>
+                        <td><input type="text" id="senha" name="senha" placeholder="senha"></td>
+                    </tr>
+                </table>
+                <input type="submit" value="Entrar"><br>
+                <button>Esqueci minha senha</button>
+            </form>
+            <h4>Não é professor? Acesse o formulário:</h4>
+            <button>Formulário de Aluno</button>
+        </article>
+    </body>
 </body>
